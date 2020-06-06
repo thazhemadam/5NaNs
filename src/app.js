498d9418a31911752d5d683cd66fa922edd90bc4
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2020-06-06 21:37:40 +0530

    Basic Routing
    * Created basic router configurations for "edit" and "help" pages

--- src/app.js
+++ src/app.js
@@ -6,24 +6,37 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
+const ExpenseDashboard = () => (
     <div>
-        This is from my dash okay
+        This is from my dashboard
+    </div>
+);
+
+const AddExpense = () => (
+    <div>
+        Add Expense Page
     </div>
 );
 
 const ExpenseHelp = () => (
     <div>
-        Hellloo
+        Help Page
     </div>
-)
+);
+
+const EditExpense = () => (
+    <div>
+        Edit your Expense
+    </div>
+);
 
 const routes = (
     <BrowserRouter>
-        <div>
-        
-        <Route path = '/add' component = {ExpenseHelp} exact={true}/>
-        <Route path = '/' component = {ExpenseDashboardPage} exact={true}/>
+        <div> 
+        <Route path = '/add' component = {AddExpense} exact={true}/>
+        <Route path = '/edit' component= {EditExpense} exact = {true} />
+        <Route path = '/help' component = {ExpenseHelp} exact={true} />
+        <Route path = '/' component = {ExpenseDashboard} exact={true}/>
         </div>
     </BrowserRouter>
 );
