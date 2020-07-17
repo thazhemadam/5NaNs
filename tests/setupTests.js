f2f44354bdf7310b5c9f0f7376b9163c6057aaf7
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2020-07-17 17:43:01 +0530

    Environment Variables, & Isolated Database for Testing
    *   The env files that must be created are .env_development, and .env_test.
    *   .env_development is required for development mode, and .env_test is required for running testcases.
    *   The format of the .env files as expected can be found in config/sample_env.env

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -2,6 +2,9 @@
 
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import * as path from 'path';
+
+require("dotenv").config({ path: '.env.test'})
 
 Enzyme.configure({
     adapter: new Adapter()
