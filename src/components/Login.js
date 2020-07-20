00683300ca4c31f098b012b641e4a35d04807c64
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2020-07-21 03:32:09 +0530

    Styling - Login Page
    The Login page and it's corresponding components have been styled.

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,19 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const Login = ({startLogin}) => (
-    <div>
-        <button onClick={startLogin}>Login</button>
+    <div className='box-layout'>
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">5NaNs</h1>
+            <p className="box-layout__title">No more NaNs. <br/> Manage your finances and keep your numbers straight!<br/></p>
+            
+            
+            <div className="google-btn" onClick={startLogin}>
+                <div className="google-icon-wrapper">
+                    <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+                </div>
+                <p className="btn-text"><b>Sign in with Google</b></p>
+            </div>
+        </div>
     </div>
 );
 
